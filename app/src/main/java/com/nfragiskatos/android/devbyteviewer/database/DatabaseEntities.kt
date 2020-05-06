bfe14c25e5ae0fe006e696c046d44eaba1627083
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-05 20:53:13 -0700

    - Created a new Room database entity to store Video objects - Created an extension function to map from the database entity to our domain model

--- app/src/main/java/com/nfragiskatos/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/nfragiskatos/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,42 @@
 
 package com.nfragiskatos.android.devbyteviewer.database
 
+import android.provider.MediaStore
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.nfragiskatos.android.devbyteviewer.domain.Video
+
+/**
+ * Defining a Room database entity
+ */
+@Entity
+data class DatabaseVideo constructor(
+        @PrimaryKey
+        val url: String,
+        val updated: String,
+        val title: String,
+        val description: String,
+        val thumbnail: String
+)
+
+/**
+ * Extension function to convert our database entity to a domain model
+ *
+ * Right now it's just a 1-to-1 mapping, but it's good practice anyway to separate the layers so if we do need to update the
+ * mapping to something more complex.
+ *
+ * Also, if we were to just use the database entity everywhere in our app then we might have to change each use of the entity
+ * object. This way we only have to change it here in this one spot.
+ */
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return map {
+        Video(
+                url = it.url,
+                title = it.title,
+                description = it.description,
+                updated = it.updated,
+                thumbnail = it.thumbnail
+        )
+    }
+}
+
