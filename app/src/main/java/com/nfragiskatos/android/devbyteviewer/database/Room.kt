810d64fb2a662112f3fbbdadc529ed1568dabf46
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-24 09:35:20 -0700

    - Created DAO in our DB for the video model

--- app/src/main/java/com/nfragiskatos/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/nfragiskatos/android/devbyteviewer/database/Room.kt
@@ -16,3 +16,18 @@
  */
 
 package com.nfragiskatos.android.devbyteviewer.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query(value = "select * from databasevideo")
+    fun getVideos() : LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
